c99187599081e494fe93cebbb813fd92ef8fdafc
LeaLenke <lea.lenke@fau.de>
2022-03-03 17:45:19 +0100

    Define function that prints quasi-polynomials mathematically

--- quasiPolynomial.py
+++ quasiPolynomial.py
@@ -14,15 +14,24 @@ class Monomial:
         """The monomials can be printed in the mathematical form."""
         output = []
         if len(self.coefficients) == 0:
+            # Check whether the monomial is empty.
             return '0'
         elif len(self.coefficients) == 1:
+            # Check whether the monomial contains only the constant term.
             return str(self.coefficients[0])
         else:
-            output.append(str(self.coefficients[0]))
-            output.append(str(self.coefficients[1]) + 'x')
-            for coefficient, exponent in list(enumerate(self.coefficients))[2:]:
-                output.append(str(exponent) + 'x^' + str(coefficient))
-            return '+'.join(output)
+            if self.coefficients[0] != 0:
+                # Check whether the constant term is zero to leave that away.
+                output.append(str(self.coefficients[0]))
+            if self.coefficients[1] != 0:
+                output.append(str(self.coefficients[1]) + 'x')
+            for exponent, coefficient in list(enumerate(self.coefficients))[2:]:
+                if coefficient != 0:
+                    # Check for the remaining coefficients whether they are zero to leave those away.
+                    output.append(str(coefficient) + 'x^' + str(exponent))
+            return '+'.join(output).replace('+-', '-')
+
+    # TODO: Define simplification.
 
     # TODO: Define multiplication with a scalar.
 
@@ -35,18 +44,54 @@ class Monomial:
 
 class QuasiPolynomial:
     """Data type of quasi-polynomial."""
-    def __init__(self, underlying_array: np.ndarray) -> None:
+    def __init__(self, monomial_array: np.ndarray) -> None:
         """A quasi-polynomial consists of an array of monomials."""
-        self.quasi_polynomial = underlying_array
+        self.monomials = monomial_array
 
     def __str__(self) -> str:
-        return str(self.quasi_polynomial)
+        return str(self.monomials)
         # TODO: What is "Convert method to property?"
 
     def pretty_print(self) -> str:
         """The quasi-polynomials can be printed in their mathematical form."""
-        return str(self.quasi_polynomial[0][0])
-        # TODO: Define this function.
+        output = []
+        if len(self.monomials) == 0:
+            # Check whether the quasi-polynomial is empty.
+            return '0'
+        if len(self.monomials) == 1:
+            # Check whether the quasi-polynomial contains only the first monomial.
+            return self.monomials[0].pretty_print()
+        else:
+            if self.monomials[0].pretty_print() != '0':
+                # Check whether the first monomial is zero to leave those away.
+                output.append(self.monomials[0].pretty_print())
+            if self.monomials[1].pretty_print() != '0':
+                # Check whether the second monomial is zero to leave those away.
+                if len(self.monomials[1].coefficients) == 1:
+                    # Check whether the monomial contains only the constant term to leave away the brackets.
+                    if self.monomials[1].coefficients == 1:
+                        # Check whether the monomial contains only 1 to leave that away.
+                        output.append('exp(-x)')
+                    else:
+                        output.append(self.monomials[1].pretty_print() + 'exp(-x)')
+                else:
+                    output.append('(' + self.monomials[1].pretty_print() + ')exp(-x)')
+            for exponent, monomial in list(enumerate(self.monomials))[2:]:
+                if monomial.pretty_print() != '0':
+                    # Check for the remaining monomials whether they are zero to leave those away.
+                    if len(monomial.coefficients) == 1:
+                        # Check for the remaining monomials whether they contain only the constant term to leave away
+                        # the brackets.
+                        if monomial.coefficients == 1:
+                            # Check for the remaining monomials whether they contain only 1 to leave that away.
+                            output.append('exp(-' + str(exponent) + 'x)')
+                        else:
+                            output.append(monomial.pretty_print() + 'exp(-' + str(exponent) + 'x)')
+                    else:
+                        output.append('(' + monomial.pretty_print() + ')exp(-' + str(exponent) + 'x)')
+            return '+'.join(output).replace('+-', '-')
+
+    # TODO: Define simplification.
 
     # TODO: Define multiplication with a scalar.
 
@@ -58,7 +103,8 @@ class QuasiPolynomial:
 
 
 def test_main():
-    qp = QuasiPolynomial(np.array([[2]]))
-    print(qp)
+    qp = QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([])),
+                                      Monomial(np.array([4]))]))
     print(qp.pretty_print())
+    print(qp.monomials[1].coefficients)
 
--- test_quasiPolynomial.py
+++ test_quasiPolynomial.py
@@ -9,7 +9,67 @@ class TestMonomial(TestCase):
         self.assertEqual(Monomial(np.array([])).pretty_print(), '0')
         self.assertEqual(Monomial(np.array([0])).pretty_print(), '0')
         self.assertEqual(Monomial(np.array([2])).pretty_print(), '2')
+        self.assertEqual(Monomial(np.array([-2])).pretty_print(), '-2')
         self.assertEqual(Monomial(np.array([2, 4])).pretty_print(), '2+4x')
+        self.assertEqual(Monomial(np.array([-2, -4])).pretty_print(), '-2-4x')
+        self.assertEqual(Monomial(np.array([0, 4])).pretty_print(), '4x')
         self.assertEqual(Monomial(np.array([2, 4, 8])).pretty_print(), '2+4x+8x^2')
-        # TODO: Add test for passing a quasi-polynomial that is also a monomial.
-        # TODO: Add test for passing a quasi-polynomial that is not a monomial.
+        self.assertEqual(Monomial(np.array([0, 4, 8])).pretty_print(), '4x+8x^2')
+        self.assertEqual(Monomial(np.array([2, 4, 0, 100])).pretty_print(), '2+4x+100x^3')
+
+class TestQuasiPolynomial(TestCase):
+    def test_pretty_print(self):
+        self.assertEqual(QuasiPolynomial(np.array([])).pretty_print(), '0')
+        self.assertEqual(QuasiPolynomial(np.array([Monomial(np.array([]))])).pretty_print(), '0')
+        self.assertEqual(QuasiPolynomial(np.array([Monomial(np.array([0]))])).pretty_print(), '0')
+        self.assertEqual(QuasiPolynomial(np.array([Monomial(np.array([2, 4, 8]))])).pretty_print(), '2+4x+8x^2')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2, 4, 8])), Monomial(np.array([1, 5, 25]))])).pretty_print(),
+            '2+4x+8x^2+(1+5x+25x^2)exp(-x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2, 4, 8])), Monomial(np.array([1, 5, 25])),
+                                      Monomial(np.array([3, 9]))])).pretty_print(),
+            '2+4x+8x^2+(1+5x+25x^2)exp(-x)+(3+9x)exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([3])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '2+3exp(-x)+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([0])), Monomial(np.array([3])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '3exp(-x)+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([0])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '2+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([3])),
+                                      Monomial(np.array([0]))])).pretty_print(),
+            '2+3exp(-x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([])), Monomial(np.array([3])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '3exp(-x)+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '2+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([3])),
+                                      Monomial(np.array([]))])).pretty_print(),
+            '2+3exp(-x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([1])),
+                                      Monomial(np.array([4]))])).pretty_print(),
+            '2+exp(-x)+4exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([2])), Monomial(np.array([3])),
+                                      Monomial(np.array([1]))])).pretty_print(),
+            '2+3exp(-x)+exp(-2x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([-2])), Monomial(np.array([-3]))])).pretty_print(),
+            '-2-3exp(-x)')
+        self.assertEqual(
+            QuasiPolynomial(np.array([Monomial(np.array([-2])), Monomial(np.array([-3])),
+                                      Monomial(np.array([-4]))])).pretty_print(),
+            '-2-3exp(-x)-4exp(-2x)')
