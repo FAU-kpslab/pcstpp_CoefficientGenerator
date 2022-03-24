6372dbe516dfa84c8c4425db50507b096a5bc0f0
LeaLenke <lea.lenke@fau.de>
2022-03-24 18:52:56 +0100

    Format output file

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ def main():
     # Give a unique name to every operator, so that you can distinguish them. You can take the operator index as its
     # name, provided that they are unique. If you want to pass the results to the solver, you need to take integers.
     # Otherwise, you can also take strings.
-    operators_left = ('-2 . id', '0 . id', '2 . id')
-    operators_right = ('id . -2', 'id . 0', 'id . 2')
+    operators_left = ('-2.id', '-1.id', '0.id', '1.id', '2.id')
+    operators_right = ('id.-2', 'id.-1', 'id.0', 'id.1', 'id.2')
     # Enter the operator indices. In Andi's case, enter the unperturbed energy differences caused by the operators.
-    translation = dict([('-2 . id', -2), ('0 . id', 0), ('2 . id', 2), ('id . -2', -2), ('id . 0', 0), ('id . 2', 2)])
+    translation = {
+        '-2.id': -2,
+        '-1.id': -1,
+        '0.id': 0,
+        '1.id': 1,
+        '2.id': 2,
+        'id.-2': -2,
+        'id.-1': -1,
+        'id.0': 0,
+        'id.1': 1,
+        'id.2': 2
+    }
     # Introduce band-diagonality.
     max_energy = 2
 
@@ -19,14 +30,18 @@ def main():
     collection = coefficientFunction.FunctionCollection(translation, max_energy)
 
     # Manually insert the solution for the coefficient functions with non-vanishing starting condition.
-    collection[(('-2 . id',), ())] = qp.new([[1]])
-    collection[(('0 . id',), ())] = qp.new([[1]])
-    collection[(('2 . id',), ())] = qp.new([[1]])
-    collection[((), ('id . -2',))] = qp.new([[-1]])
-    collection[((), ('id . 0',))] = qp.new([[-1]])
-    collection[((), ('id . 2',))] = qp.new([[-1]])
+    collection[(('-2.id',), ())] = qp.new([[1]])
+    collection[(('0.id',), ())] = qp.new([[1]])
+    collection[(('2.id',), ())] = qp.new([[1]])
+    collection[((), ('id.-2',))] = qp.new([[-1]])
+    collection[((), ('id.0',))] = qp.new([[-1]])
+    collection[((), ('id.2',))] = qp.new([[-1]])
+    collection[(('-1.id',), ('id.1',))] = qp.new([[1]])
+    collection[(('1.id', '-1.id'), ())] = qp.new([[-1/2]])
+    collection[((), ('id.-1', 'id.1'))] = qp.new([[-1/2]])
 
-    max_order = 4
+    # TODO: The fractions data type overflows in max_order 8.
+    max_order = 8
     for order in range(max_order + 1):
         for order_left in range(order + 1):
             order_right = order - order_left
@@ -43,11 +58,17 @@ def main():
                                                                                          translation, max_energy)
     # print(collection.pretty_print())
     # Print the block-diagonal operator sequences yielding the effective operator.
-    with open("result.txt", "w") as result:
+    with open("TCDissipationO6.txt", "w") as result:
         for sequence in collection.keys():
             if energy(coefficientFunction.sequence_to_indices(sequence, translation)) == 0:
-                if collection[sequence].function.get_constant() != 0:
-                    print(str(sequence) + ': ' + str(collection[sequence].function.get_constant()), file=result)
+                resulting_constant = collection[sequence].function.get_constant()
+                if resulting_constant != 0:
+                    # Invert operator sequence, because the Solver thinks from left to right.
+                    inverted_sequence = [operator for operator in sequence[0][::-1]] + [operator for operator in
+                                                                                        sequence[1][::-1]]
+                    output = [str(len(sequence[0]) + len(sequence[1]))] + inverted_sequence + [
+                        str(resulting_constant.numerator), str(resulting_constant.denominator)]
+                    print(' '.join(output), file=result)
         result.close()
 
 
--- test_coefficientFunction.py
+++ test_coefficientFunction.py
@@ -3,54 +3,36 @@ import unittest
 from coefficientFunction import FunctionCollection
 from quasiPolynomial import QuasiPolynomial as qp
 
+translation = dict([('-2.id', -2), ('0.id', 0), ('2.id', 2), ('id.-2', -2), ('id.0', 0), ('id.2', 2)])
+collection = FunctionCollection(translation, max_energy=2)
+collection[(('-2.id',), ())] = qp.new([[1]])
+collection[(('0.id',), ())] = qp.new([[1]])
+collection[(('2.id',), ())] = qp.new([[1]])
+collection[((), ('id.-2',))] = qp.new([[-1]])
+collection[((), ('id.0',))] = qp.new([[-1]])
+collection[((), ('id.2',))] = qp.new([[-1]])
+
 
 class TestFunctionCollection(unittest.TestCase):
+
     def test_contains(self):
-        translation = dict([('-2.id', -2), ('0.id', 0), ('2.id', 2), ('id.-2', -2), ('id.0', 0), ('id.2', 2)])
-        collection = FunctionCollection(translation, max_energy=2)
-        collection[(('-2.id',), ())] = qp.new([[1]])
-        collection[(('0.id',), ())] = qp.new([[1]])
-        collection[(('2.id',), ())] = qp.new([[1]])
-        collection[((), ('id.-2',))] = qp.new([[-1]])
-        collection[((), ('id.0',))] = qp.new([[-1]])
-        collection[((), ('id.2',))] = qp.new([[-1]])
         self.assertTrue((('-2.id',), ()) in collection)
         self.assertFalse((('1',), ()) in collection)
 
     def test_keys(self):
-        translation = dict([('-2.id', -2), ('0.id', 0), ('2.id', 2), ('id.-2', -2), ('id.0', 0), ('id.2', 2)])
-        collection = FunctionCollection(translation, max_energy=2)
-        collection[(('-2.id',), ())] = qp.new([[1]])
-        collection[(('0.id',), ())] = qp.new([[1]])
-        collection[(('2.id',), ())] = qp.new([[1]])
-        collection[((), ('id.-2',))] = qp.new([[-1]])
-        collection[((), ('id.0',))] = qp.new([[-1]])
-        collection[((), ('id.2',))] = qp.new([[-1]])
         self.assertEqual(collection.keys(),
                          [(('-2.id',), ()), (('0.id',), ()), (('2.id',), ()), ((), ('id.-2',)), ((), ('id.0',)),
                           ((), ('id.2',))])
 
     def test_print(self):
-        translation = dict([('-2.id', -2), ('0.id', 0), ('1.id', 1), ('2.id', 2)])
-        collection = FunctionCollection(translation, max_energy=2)
-        collection[(('-2.id',), ())] = qp.new([[1]])
-        collection[(('2.id',), ())] = qp.new([[1]])
-        collection[(('1.id', '0.id'), ())] = qp.zero()
         self.assertEqual(str(collection),
-                         str(["(('-2.id',), ()): [[Fraction(1, 1)]]", "(('2.id',), ()): [[Fraction(1, 1)]]",
-                              "(('1.id', '0.id'), ()): []"]))
+                         str(["(('-2.id',), ()): [[Fraction(1, 1)]]", "(('0.id',), ()): [[Fraction(1, 1)]]",
+                              "(('2.id',), ()): [[Fraction(1, 1)]]", "((), ('id.-2',)): [[Fraction(-1, 1)]]",
+                              "((), ('id.0',)): [[Fraction(-1, 1)]]", "((), ('id.2',)): [[Fraction(-1, 1)]]"]))
 
 
 class TestDifferentialEquation(unittest.TestCase):
     def test_differential_equation(self):
-        translation = dict([('-2.id', -2), ('0.id', 0), ('2.id', 2), ('id.-2', -2), ('id.0', 0), ('id.2', 2)])
-        collection = FunctionCollection(translation, max_energy=2)
-        collection[(('-2.id',), ())] = qp.new([[1]])
-        collection[(('0.id',), ())] = qp.new([[1]])
-        collection[(('2.id',), ())] = qp.new([[1]])
-        collection[((), ('id.-2',))] = qp.new([[-1]])
-        collection[((), ('id.0',))] = qp.new([[-1]])
-        collection[((), ('id.2',))] = qp.new([[-1]])
         self.assertEqual(collection[(('2.id', '-2.id'), ())].function, qp.new([[1/2], [], [], [], [-1/2]]))
         self.assertEqual(collection[(('0.id',), ('id.-2',))].function, qp.zero())
 
