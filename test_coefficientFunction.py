10f4df074055e50d10c94f5a6abe76a0952023da
Andreas Schellenberger <andreas.schellenberger@fau.de>
2022-06-09 12:43:27 +0200

    Updated coefficient function tests to work with new sequence format #6

--- test_coefficientFunction.py
+++ test_coefficientFunction.py
@@ -5,12 +5,12 @@ from quasiPolynomial import QuasiPolynomial as qp
 
 translation = dict([('-2.id', -2), ('0.id', 0), ('2.id', 2), ('id.-2', -2), ('id.0', 0), ('id.2', 2)])
 collection = FunctionCollection(translation)
-collection[(('-2.id',), ())] = qp.new([[1]])
-collection[(('0.id',), ())] = qp.new([[1]])
-collection[(('2.id',), ())] = qp.new([[1]])
-collection[((), ('id.-2',))] = qp.new([[-1]])
-collection[((), ('id.0',))] = qp.new([[-1]])
-collection[((), ('id.2',))] = qp.new([[-1]])
+collection[[('-2.id',), ()]] = qp.new([[1]])
+collection[[('0.id',), ()]] = qp.new([[1]])
+collection[[('2.id',), ()]] = qp.new([[1]])
+collection[[(), ('id.-2',)]] = qp.new([[-1]])
+collection[[(), ('id.0',)]] = qp.new([[-1]])
+collection[[(), ('id.2',)]] = qp.new([[-1]])
 
 
 class TestFunctionCollection(unittest.TestCase):
@@ -21,14 +21,14 @@ class TestFunctionCollection(unittest.TestCase):
 
     def test_keys(self):
         self.assertEqual(collection.keys(),
-                         [(('-2.id',), ()), (('0.id',), ()), (('2.id',), ()), ((), ('id.-2',)), ((), ('id.0',)),
-                          ((), ('id.2',))])
+                         [[('-2.id',), ()], [('0.id',), ()], [('2.id',), ()], [(), ('id.-2',)], [(), ('id.0',)],
+                          [(), ('id.2',)]])
 
     def test_print(self):
         self.assertEqual(str(collection),
-                         str(["(('-2.id',), ()): [['1']]", "(('0.id',), ()): [['1']]",
-                              "(('2.id',), ()): [['1']]", "((), ('id.-2',)): [['-1']]",
-                              "((), ('id.0',)): [['-1']]", "((), ('id.2',)): [['-1']]"]))
+                         str(["[('-2.id',), ()]: [['1']]", "[('0.id',), ()]: [['1']]",
+                              "[('2.id',), ()]: [['1']]", "[(), ('id.-2',)]: [['-1']]",
+                              "[(), ('id.0',)]: [['-1']]", "[(), ('id.2',)]: [['-1']]"]))
 
 
 class TestDifferentialEquation(unittest.TestCase):
